ecb7fa7a3c8f288286424c4f758c7be01b4ca459
kebabzter <gabzterthesecond@gmail.com>
2026-03-27 12:09:09 +0100

    fixing folder struct

---
+++ target/debug/deps/grrs-41970fe24fda222d.d
@@ -0,0 +1,5 @@
+/home/gabriel/dev/uni/year-2/rusty-rails/grrs/target/debug/deps/grrs-41970fe24fda222d.d: src/main.rs
+
+/home/gabriel/dev/uni/year-2/rusty-rails/grrs/target/debug/deps/grrs-41970fe24fda222d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/grrs.d
@@ -0,0 +1,1 @@
+/home/gabriel/dev/uni/year-2/rusty-rails/grrs/target/debug/grrs: /home/gabriel/dev/uni/year-2/rusty-rails/grrs/src/main.rs
