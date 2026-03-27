d75b47b5ae1b15e7e921ad96a22ac8e8808a68cd
kebabzter <gabzterthesecond@gmail.com>
2026-03-27 16:37:55 +0100

    Trains now properly show their length instead of being infinite + switches on the map are now rendered
    Co-authored-by: Name <mane0003@hz.nl>

--- src/engine.rs
+++ src/engine.rs
@@ -6,6 +6,12 @@ use std::{
     time::{Duration, Instant},
 };
 
+// pub fn switch(mut train:Train, mut track: Vec<Vec<String>>){
+//     if(track[train.position + 1][train.currLane] == "="){
+//         train.currLane = train.to;
+//     }
+// }
+
 pub fn run(mut trains_map: HashMap<usize, Train>, mut track: Vec<Vec<String>>) {
     let target_fps = 60;
     let frame_duration = Duration::from_millis(1000 / target_fps);
@@ -15,12 +21,15 @@ pub fn run(mut trains_map: HashMap<usize, Train>, mut track: Vec<Vec<String>>) {
 
         // --- 1. UPDATE ---
         for (_, train) in &mut trains_map {
-            train.timer += 1;
+            train.timer += 1 * train.speed;
 
             if train.timer >= train.cooldown {
+                if train.position - train.wagons >= 0 {
+                    track[train.currLane as usize][(train.position - train.wagons) as usize] = "=".to_string();
+                }
                 track[train.currLane as usize][train.position as usize] = "#".to_string();
                 train.train_update();
-                train.timer = 0;
+                train.timer = 0
             }
         }
 
--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,15 @@ fn main() {
 
     const ROW: usize = 60;
     const COL: usize = 7;
+
+    const STARTING_COL:usize = (ROW/2) - 3;
+
     let mut track: Vec<Vec<String>> = vec![vec!["=".to_string(); ROW]; COL];
+    for lane in 0..COL {
+        for pos in STARTING_COL..(STARTING_COL + 6) {
+            track[lane][pos] = "x".to_string();
+        }
+    }
 
     println!("Please enter how many trains will run in this simulation:");
 
